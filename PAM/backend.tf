31b5949f1865d201d56ab7bdb59e499d51954191
Tanmay Bhambure <38174419+tanbham@users.noreply.github.com>
2025-08-16 22:13:38 +0530

    Update backend.tf

--- PAM/backend.tf
+++ PAM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket = "bkt-common-srv-terraform-app-infra-state-as1"
-    prefix = "terraform/mc-nonprod/mc-std-svc-uat/pam/state"
+    bucket = "bucket-name"
+    prefix = "terraform/pam/state"
   }
 }
